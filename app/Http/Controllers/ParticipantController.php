cb6ae39d9afb025f4066d6a1776af200b8206401
matzgans <rahmatlahay84@gmail.com>
2024-08-27 22:31:58 +0800

    custom tickets

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -80,7 +80,7 @@ class ParticipantController extends Controller
 
             // Buat ticket_number unik
             do {
-                $ticket_number = 'DIARY' . str_pad(rand(1, 9999), 4, '0', STR_PAD_LEFT);
+                $ticket_number = 'DIARY' . str_pad(rand(0, 9999999999), 10, '0', STR_PAD_LEFT);
             } while (Participant::where('ticket_number', $ticket_number)->exists());
 
             // Simpan data ke database
