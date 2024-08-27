ca17ff53e6f997f301e034d071a78b47b1c4e0f9
fzrsahi <fazrul.anugrah17@gmail.com>
2024-08-27 23:14:41 +0800

    add tambah data buttin

--- resources/views/pages/undian/index.blade.php
+++ resources/views/pages/undian/index.blade.php
@@ -6,6 +6,18 @@
                     {{ __('Undian') }}
                 </div>
                 <div class="w-ful">
+                    <button
+                        class="rounded-lg bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto ml-5"
+                        type="submit">
+
+                        <a href="">Tambah Data Hadiah</a>
+                    </button>
+                    <button
+                        class="rounded-lg bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto ml-5"
+                        type="submit">
+
+                        <a href="">Lihat Jumlah Pemenang</a>
+                    </button>
                     <form class="mx-auto max-w-full p-5" action="{{ route('undian.store') }}" method="post">
                         @csrf
                         <div class="grid grid-cols-2 gap-4">
